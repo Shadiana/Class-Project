80c06c61156043f9e80acafa5a4dc712b7adb595
Shadiana <147126017+Shadiana@users.noreply.github.com>
2024-09-10 00:38:58 -0500

    Add files via upload

---
+++ Ice Cream.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std; 
+
+int main()
+{
+	cout << "What is your favorite flavor of ice cream?" <<; 
+	cout << "My favorite flavor of ice cream is strawberry. What's yours?" <<;
+	cout << "Chocolate!" << endl;
+	
+	return 0;
+}
