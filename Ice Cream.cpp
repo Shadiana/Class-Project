7a0118bf5bc2ecc9ba6fce8b654e1e5cb9526fdb
Shadiana <147126017+Shadiana@users.noreply.github.com>
2024-09-10 00:59:25 -0500

    Ice Cream.cpp

--- Ice Cream.cpp
+++ Ice Cream.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main()
 {
-	cout << "What is your favorite flavor of ice cream?" << endl; 
-	cout << "My favorite flavor of ice cream is strawberry. What's yours?" << endl;
-	cout << "Chocolate!" << endl;
+	cout << "What is your favorite flavor of ice cream?" <<; 
+	cout << "My favorite flavor of ice cream is strawberry. What's yours?" <<;
+	cout << "Chocolate!" <<;
 	
 	return 0;
 }
