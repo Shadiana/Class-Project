e6e292623a815a1602e4b808bcebf00e7f66e4d3
Casper0990 <orangepresidents@gmail.com>
2024-09-10 00:51:51 -0500

    Improved Ice Cream.cpp

--- Ice Cream.cpp
+++ Ice Cream.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-	cout << "What is your favorite flavor of ice cream?" <<; 
-	cout << "My favorite flavor of ice cream is strawberry. What's yours?" <<;
+	cout << "What is your favorite flavor of ice cream?" << endl; 
+	cout << "My favorite flavor of ice cream is strawberry. What's yours?" << endl;
 	cout << "Chocolate!" << endl;
 	
 	return 0;
